39e05a83c27d234c55b12faceecb07915cd60e4a
ignasiusdandy <ignasiusdandy123@gmail.com>
2026-03-27 00:07:31 +0800

    Maps sementara tanpa database

--- app/src/main/java/com/example/skripsi/MapsFragment.java
+++ app/src/main/java/com/example/skripsi/MapsFragment.java
@@ -20,7 +20,9 @@ import org.osmdroid.views.overlay.infowindow.InfoWindow;
 public class MapsFragment extends Fragment {
     private MapView map;
     double tinggiAir = 20;
-    String status = "Bahaya";
+    String status = "Resiko Sedang";
+    String lokasi = "";
+    String title = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -36,24 +38,22 @@ public class MapsFragment extends Fragment {
         map.setTileSource(TileSourceFactory.MAPNIK);
 
         // posisi
-        GeoPoint startPoint = new GeoPoint(-3.296702248237609, 114.58375641904426);
+        GeoPoint jalanDatang = new GeoPoint(-3.296702248237609, 114.58375641904426);
+        GeoPoint jalanPulang = new GeoPoint(-3.2955525424546877, 114.58486568269987);
 
-        map.getController().setZoom(18.0);
-        map.getController().setCenter(startPoint);
+        map.getController().setZoom(19.0);
+        map.getController().setCenter(jalanDatang);
+        Marker markerDatang = new Marker(map);
+        markerDatang.setPosition(jalanDatang);
 
-        Marker marker = new Marker(map);
-        marker.setPosition(startPoint);
-        marker.setInfoWindow(new InfoWindow(R.layout.info_window_custom, map){
+        markerDatang.setInfoWindow(new InfoWindow(R.layout.info_window_custom, map){
             @Override
             public void onOpen(Object item){
                 TextView tvtittle = mView.findViewById(R.id.tvTittle);
-                TextView tvinfo = mView.findViewById(R.id.tvInfo);
+                TextView tvinfotinggi = mView.findViewById(R.id.tvInfoTinggi);
                 TextView btndetail = mView.findViewById(R.id.btn_detail);
-
                 tvtittle.setText("Jalan Datang");
-                tvinfo.setText("Tinggi Air: " + tinggiAir + "cm\n" +
-                        "Status: " + status
-                );
+                tvinfotinggi.setText(tinggiAir + " Cm");
 
                 btndetail.setOnClickListener(v -> {
                     Intent intent = new Intent(getActivity(), DashboardActivity.class);
@@ -65,11 +65,52 @@ public class MapsFragment extends Fragment {
             public void onClose() {}
         });
 
-        marker.setOnMarkerClickListener((m, mapView) -> {
+        markerDatang.setOnMarkerClickListener((m, mapView) -> {
+            InfoWindow.closeAllInfoWindowsOn(map);
             m.showInfoWindow();
             return true;
         });
-        map.getOverlays().add(marker);
+        map.getOverlays().add(markerDatang);
+
+
+
+        Marker markerPulang = new Marker(map);
+        markerPulang.setPosition(jalanPulang);
+
+        markerPulang.setInfoWindow(new InfoWindow(R.layout.info_window_custom, map){
+            @Override
+            public void onOpen(Object item){
+                TextView tvtittle = mView.findViewById(R.id.tvTittle);
+                TextView tvinfotinggi = mView.findViewById(R.id.tvInfoTinggi);
+                TextView btndetail = mView.findViewById(R.id.btn_detail);
+                tvtittle.setText("Jalan Pulang");
+                tvinfotinggi.setText(tinggiAir + " Cm");
+
+                btndetail.setOnClickListener(v -> {
+                    Intent intent = new Intent(getActivity(), DashboardActivity.class);
+                    startActivity(intent);
+                });
+
+            }
+
+            @Override
+            public void onClose() {}
+        });
+
+        markerDatang.setOnMarkerClickListener((m, mapView) -> {
+            InfoWindow.closeAllInfoWindowsOn(map);
+            m.showInfoWindow();
+            return true;
+        });
+        map.getOverlays().add(markerPulang);
+
+        // Hilang saat klik map
+        map.setOnTouchListener((v, event) -> {
+            if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+                org.osmdroid.views.overlay.infowindow.InfoWindow.closeAllInfoWindowsOn(map);
+            }
+            return false;
+        });
 
         return view;
     }
